517614e0f75381748e619cb43c46575f18fe410c
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2018-04-11 21:33:06 +0200

    Modified agent architecture

--- agent/continuous_agent.py
+++ agent/continuous_agent.py
@@ -3,10 +3,9 @@ import numpy as np
 
 class StochasticPolicyGradientAgent():
     """
-    A Gaussian Policy Gradient based on lantunes's agent for MountainCarContinuous
-    github.com/lantunes/mountain-car-continuous
+    A Gaussian Policy Gradient based agent implementation
     """
-    def __init__(self, env, learning_rate = 0.001, discount_rate = 0.99):
+    def __init__(self, env, learning_rate = 0.0001, discount_rate = 0.99):
         self._optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
         self._sess = tf.Session()
         self._env = env
@@ -16,8 +15,6 @@ class StochasticPolicyGradientAgent():
         self._reward_buffer = []
         self._action_buffer = []
         
-        self._phi_hidden = 128
-        self._sigma_hidden = 32
         
         state_dim = env.observation_space.shape[0] * env.observation_space.shape[1]
         
@@ -27,47 +24,73 @@ class StochasticPolicyGradientAgent():
         
         state = tf.layers.flatten(self._states)
         init = tf.contrib.layers.xavier_initializer()
-    # policy parameters
-        self._mu_theta = tf.get_variable("mu_theta", [self._phi_hidden, 1],
-                                         initializer=init)
-        self._sigma_theta = tf.get_variable("sigma_theta", [self._sigma_hidden],
-                                                initializer=init)
         
         # neural featurizer parameters
-        self._W1 = tf.get_variable("W1", [state_dim, self._phi_hidden],
-                                   initializer=init)
-        self._b1 = tf.get_variable("b1", [self._phi_hidden],
-                                   initializer=tf.constant_initializer(0))
-        self._h1 = tf.nn.tanh(tf.matmul(state, self._W1) + self._b1)
-        self._W2 = tf.get_variable("W2", [self._phi_hidden, self._phi_hidden],
-                                   initializer=init)
-        self._b2 = tf.get_variable("b2", [self._phi_hidden],
-                                   initializer=tf.constant_initializer(0))
-        self._phi = tf.nn.tanh(tf.matmul(self._h1, self._W2) + self._b2)
+        h1 = 128
+        h2 = 64
+        h3 = 32
         
-        self._mu = tf.tanh(tf.matmul(self._phi, self._mu_theta))
+        mu_hidden = tf.layers.dense(state, h1, 
+                                    activation = tf.sigmoid, 
+                                    name = 'dense', 
+                                    kernel_initializer=init)
+        mu_hidden_2 = tf.layers.dense(mu_hidden, h2, 
+                                      activation = tf.sigmoid, 
+                                      name = 'dense_1', 
+                                      kernel_initializer=init)
+        mu_hidden_3 = tf.layers.dense(mu_hidden_2, h3, 
+                                      activation = tf.sigmoid, 
+                                      name = 'dense_2', 
+                                      kernel_initializer=init)
+        self._mu = tf.layers.dense(mu_hidden_3, 1, 
+                                   activation = tf.tanh, 
+                                   name = 'mu', 
+                                   kernel_initializer=init)
+        self._mu = tf.squeeze(self._mu)
         
-        self._sigma = tf.reduce_sum(self._sigma_theta)
-        self._sigma = tf.exp(self._sigma)
-            
+
+        # Building sigma Model
+        
+        sig_hidden = tf.layers.dense(state, h1, 
+                                     activation = tf.sigmoid, 
+                                     name = 'sigma_dense', 
+                                     kernel_initializer=init)
+        sig_hidden_2 = tf.layers.dense(sig_hidden, h2, 
+                                       activation = tf.sigmoid, 
+                                       name = 'sig_dense_1', 
+                                       kernel_initializer=init)
+        sig_hidden_3 = tf.layers.dense(sig_hidden_2, h3, 
+                                       activation = tf.sigmoid, 
+                                       name = 'sig_dense_2', 
+                                       kernel_initializer=init)
+        self._sigma = tf.layers.dense(sig_hidden_3, 1, 
+                                      activation = tf.exp, 
+                                      name = 'sigma', 
+                                      kernel_initializer=init)
+        self._sigma = tf.squeeze(self._sigma)
+        self._sigma = tf.add(self._sigma, 1e-10)
+        
+        #Sampling action from distribuition
+        self._normal_dist = tf.contrib.distributions.Normal(self._mu, self._sigma)
+        self._action = self._normal_dist.sample(1)
+        
         #Computing loss function
         
         self._discounted_rewards = tf.placeholder(tf.float32, (None, 1), name="discounted_rewards")
         self._taken_actions = tf.placeholder(tf.float32, (None, 1), name="taken_actions")
         
-        self._loss = -tf.log(1e-5 + tf.sqrt(1/(2 * np.pi * self._sigma**2)) 
-                             * tf.exp(-(self._taken_actions - self._mu)**2/(2 * self._sigma**2))) * self._discounted_rewards
+        self._loss = -tf.log(1e-5 + self._normal_dist.prob(self._taken_actions)) * self._discounted_rewards
         
         self._train_op = self._optimizer.minimize(self._loss)        
         
         self._sess.run(tf.global_variables_initializer())
                 
     def act(self, state):
-        mu, sigma = self._sess.run([self._mu, self._sigma], feed_dict={
+        mu, sigma, action = self._sess.run([self._mu, self._sigma, self._action], feed_dict={
             self._states: state})
-        action = np.random.normal(mu, sigma)
         action = np.clip(action, self._env.action_space.low[0], self._env.action_space.high[0])
         print("Sigma: {}, Mu: {}, Action: {}".format(sigma, mu, action))
+        
         return action[0]
     
     def train(self): 
@@ -92,6 +115,8 @@ class StochasticPolicyGradientAgent():
         self._state_buffer  = []
         self._reward_buffer = []
         self._action_buffer = []
+        
+        return loss
     
     def store_step(self, action, state, reward):
         self._state_buffer.append(state)
