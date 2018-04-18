0cb827cd8f64f2a686f0604d23d9edf98fb11d8c
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2018-04-19 00:19:10 +0200

    Improved model and fixed loss output bug

--- agent/continuous_agent.py
+++ agent/continuous_agent.py
@@ -5,8 +5,11 @@ class StochasticPolicyGradientAgent():
     """
     A Gaussian Policy Gradient based agent implementation
     """
-    def __init__(self, env, learning_rate = 0.0001, discount_rate = 0.99):
-        self._optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+    def __init__(self, env, learning_rate = None, discount_rate = 0.99):
+        if learning_rate is None:
+            self._optimizer = tf.train.AdamOptimizer()
+        else:
+            self._optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
         self._sess = tf.Session()
         self._env = env
         
@@ -16,13 +19,13 @@ class StochasticPolicyGradientAgent():
         self._action_buffer = []
         
         
-        state_dim = env.observation_space.shape[0] * env.observation_space.shape[1]
-        
+        state_dim = np.prod(np.array(env.observation_space.shape))
+
         self._states = tf.placeholder(tf.float32, 
-                                      shape=(None, env.observation_space.shape[0], env.observation_space.shape[1]), 
+                                      shape=(None, state_dim), 
                                       name="states")
         
-        state = tf.layers.flatten(self._states)
+
         init = tf.contrib.layers.xavier_initializer()
         
         # neural featurizer parameters
@@ -30,9 +33,9 @@ class StochasticPolicyGradientAgent():
         h2 = 64
         h3 = 32
         
-        mu_hidden = tf.layers.dense(state, h1, 
+        mu_hidden = tf.layers.dense(self._states, h1, 
                                     activation = tf.sigmoid, 
-                                    name = 'dense', 
+                                    name = 'dense_0', 
                                     kernel_initializer=init)
         mu_hidden_2 = tf.layers.dense(mu_hidden, h2, 
                                       activation = tf.sigmoid, 
@@ -42,8 +45,12 @@ class StochasticPolicyGradientAgent():
                                       activation = tf.sigmoid, 
                                       name = 'dense_2', 
                                       kernel_initializer=init)
-        self._mu = tf.layers.dense(mu_hidden_3, 1, 
-                                   activation = tf.tanh, 
+        mu_hidden_4 = tf.layers.dense(mu_hidden_3, h3, 
+                                      activation = tf.sigmoid, 
+                                      name = 'dense_3', 
+                                      kernel_initializer=init)
+        self._mu = tf.layers.dense(mu_hidden, 1,
+                                   activation = tf.tanh,
                                    name = 'mu', 
                                    kernel_initializer=init)
         self._mu = tf.squeeze(self._mu)
@@ -51,9 +58,9 @@ class StochasticPolicyGradientAgent():
 
         # Building sigma Model
         
-        sig_hidden = tf.layers.dense(state, h1, 
+        sig_hidden = tf.layers.dense(self._states, h1, 
                                      activation = tf.sigmoid, 
-                                     name = 'sigma_dense', 
+                                     name = 'sigma_dense_0', 
                                      kernel_initializer=init)
         sig_hidden_2 = tf.layers.dense(sig_hidden, h2, 
                                        activation = tf.sigmoid, 
@@ -68,19 +75,20 @@ class StochasticPolicyGradientAgent():
                                       name = 'sigma', 
                                       kernel_initializer=init)
         self._sigma = tf.squeeze(self._sigma)
-        self._sigma = tf.add(self._sigma, 1e-10)
+        self._sigma = tf.add(self._sigma, 1e-5)
         
         #Sampling action from distribuition
+        
         self._normal_dist = tf.contrib.distributions.Normal(self._mu, self._sigma)
-        self._action = self._normal_dist.sample(1)
+        self._action = self._normal_dist.sample()
         
         #Computing loss function
         
         self._discounted_rewards = tf.placeholder(tf.float32, (None, 1), name="discounted_rewards")
         self._taken_actions = tf.placeholder(tf.float32, (None, 1), name="taken_actions")
         
-        self._loss = -tf.log(1e-5 + self._normal_dist.prob(self._taken_actions)) * self._discounted_rewards
-        
+        self._loss = -tf.log(1e-10 + self._normal_dist.prob(self._taken_actions)) * self._discounted_rewards
+                                                            
         self._train_op = self._optimizer.minimize(self._loss)        
         
         self._sess.run(tf.global_variables_initializer())
@@ -91,37 +99,32 @@ class StochasticPolicyGradientAgent():
         action = np.clip(action, self._env.action_space.low[0], self._env.action_space.high[0])
         print("Sigma: {}, Mu: {}, Action: {}".format(sigma, mu, action))
         
-        return action[0]
+        return action
     
     def train(self): 
-        rewards = []
-        rewards.append(self._discount_rewards().tolist())
-        rewards = [[r] for r in rewards[0]]
+        rewards = self._discount_rewards().tolist()
         norm_rewards = rewards
         norm_rewards -= np.mean(rewards)
-        norm_rewards /= np.std(rewards)
+        #norm_rewards /= np.std(rewards)
+        rewards = [[r] for r in rewards]
+        norm_rewards = [[r] for r in norm_rewards]
         feed_dict={
             self._states: self._state_buffer,
             self._discounted_rewards: rewards,
             self._taken_actions: self._action_buffer}
-        
-        loss = self._sess.run(self._loss, feed_dict=feed_dict)
-        
+        #print(feed_dict)
         self._sess.run([self._train_op], feed_dict=feed_dict)
-        
-        print(loss)
+        #print(self._sess.run(self._loss, feed_dict=feed_dict))
         
         #After applying gradients
         self._state_buffer  = []
         self._reward_buffer = []
         self._action_buffer = []
-        
-        return loss
     
     def store_step(self, action, state, reward):
         self._state_buffer.append(state)
-        self._reward_buffer.append(reward)
-        self._action_buffer.append(action)
+        self._reward_buffer.append(np.array(reward))
+        self._action_buffer.append(np.array([action]))
         
     def _discount_rewards(self):
         r = 0
